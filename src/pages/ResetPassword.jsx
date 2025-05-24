d1eaf478aaa4071a3c236d96b55184ebf7427fef
Full Stack Developer <146775911+Huwigor@users.noreply.github.com>
2025-05-24 00:23:44 -0300

    Ajust the reset password interface

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,36 +1,62 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import HeaderAuth from '../components/headerAuth.jsx';
+import '../css/ResetPassword.css'
 
 export default function ResetPassword() {
     const { token } = useParams();
     const [password, setPassword] = useState('');
     const [msg, setMsg] = useState('');
 
+    const navigate = useNavigate()
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             await axios.post(`${import.meta.env.VITE_ROUTE_SERVER}/api/auth/resetPassword/${token}`, { password });
-            setMsg('Senha redefinida com sucesso!');
+            navigate('/')
         } catch (err) {
-            setMsg('Erro ao redefinir senha.');
+            setMsg('Aconteceu algum erro inesperado, refaça o processo por favor!');
         }
     };
 
+    const fullText = "Escolha uma nova senha!";
+          const [typedText, setTypedText] = useState('');
+          const [index, setIndex] = useState(0);
+    
+          useEffect(() => {
+              if (index < fullText.length) {
+                const timeout = setTimeout(() => {
+                  setTypedText(prev => prev + fullText.charAt(index));
+                  setIndex(prev => prev + 1);
+                }, 15);
+                return () => clearTimeout(timeout);
+              } 
+            }, [index]);
+
     return (
-        <div>
-            <h2>Redefinir Senha</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Nova senha"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Redefinir</button>
-            </form>
-            <p>{msg}</p>
-        </div>
+        <>
+           <HeaderAuth/>
+            <div className='mainReset'>
+                <div className='boxReset'>
+                    <h2>{typedText} <span className='cursor'>|</span></h2>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name='novaSenha'
+                            id='novaSenha'
+                            placeholder="Nova senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        {msg ? ( <p className='alert alert-danger span-error' style={{padding:'10px', marginBottom:'0px', marginTop:'20px'}}>{msg}</p>) : ''}
+                       
+                        <button className='mx-auto' type="submit">Redefinir</button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 }

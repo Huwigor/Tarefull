6941ca91cd7670d5ed219cbd53f0ee296b42b0a5
Full Stack Developer <146775911+Huwigor@users.noreply.github.com>
2025-07-11 15:23:43 -0300

    tiny change

--- src/pages/userRegisterStepTwo.jsx
+++ src/pages/userRegisterStepTwo.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import '../css/UserRegisterStepTwo.css'
+import '../css/userRegisterStepTwo.css'
 import HeaderAuth from '../components/headerAuth.jsx'
 
 

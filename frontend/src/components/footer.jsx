00ff711cbfc1939a62f012348a49fec7aab5beb3
Full Stack Developer <146775911+Huwigor@users.noreply.github.com>
2025-07-12 05:09:21 -0300

    tiny change in footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/icon-logo.png'
 import { Github, Linkedin } from 'lucide-react'
+import '../css/footer.css'
 
 export default function Footer(){
 
@@ -8,14 +9,10 @@ export default function Footer(){
 
     return(
         <div 
-            className='footer'
-            style={{
-                marginTop:'200px', 
-                borderRadius:'10px'
-            }}
-            
+            className='footer'   
         >
             <div 
+                className='boxIconFooter'
                 style={{
                     width:'100%', 
                     display:'flex', 
@@ -42,24 +39,24 @@ export default function Footer(){
                     }}
                 >
                     <a 
+                        className='linkGithub'
                         href={hrefGithub}
                         target='_blank'
                         style={{
                             backgroundColor:'transparent', 
                             border:'none', 
-                            color:'white',
                             marginLeft:'5px'
                         }}
                     >
                         <Github size={20}/>
                     </a>
                     <a 
+                       className='linkLinkedin'
                        href={hrefLinkedin}
                        target='_blank'
                        style={{
                             backgroundColor:'transparent', 
                             border:'none', 
-                            color:'white',
                             marginLeft:'10px'
                         }}
                     >

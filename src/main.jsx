8d468f6d91870939425b14677a0194072873cd47
Full Stack Developer <146775911+Huwigor@users.noreply.github.com>
2025-07-11 21:13:34 -0300

    tiny modify

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from 'react-router-dom'
 import { ThemeProvider } from './components/Theme.jsx'
-import router from './routes/loaderRoutes.jsx'
+import router from './components/loaderRoutes.jsx'
 
 createRoot(document.getElementById('root')).render(
 

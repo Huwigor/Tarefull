938834ba24cc7a0192ace644b687665b2a6d21cb
Full Stack Developer <146775911+Huwigor@users.noreply.github.com>
2025-07-19 16:04:02 -0300

    adjustment in the design of the forms for editing and deleting groups, tasks and subtasks

--- frontend/src/components/subTasks/formEditSubTask.jsx
+++ frontend/src/components/subTasks/formEditSubTask.jsx
@@ -50,17 +50,6 @@ export default function EditSubTask({subTarefasDescricao, tarefaId, subTarefaId,
             <button className='btnCloseEdit ms-auto' onClick={()=>subTaskEditClosed()}><X/></button>
             <form action="" onSubmit={handleEdit}>
             <p className='titleEditSubTask'>Nome da sub tarefa</p>
-            <div
-                className="char-counter"
-                style={{
-                    fontSize: '0.75rem',
-                    textAlign: 'right',
-                    marginBottom: '5px',
-                    color: descricao.length >= subTaskLength ? 'red' : '#666',
-                }}
-            >
-                {descricao.length} / {subTaskLength}
-            </div>
             <textarea 
                 type="text" 
                 id='edit' 
@@ -72,6 +61,18 @@ export default function EditSubTask({subTarefasDescricao, tarefaId, subTarefaId,
                     }
                 }}
             />
+             <div
+                className="char-counter"
+                style={{
+                    fontSize: '0.75rem',
+                    textAlign: 'right',
+                    marginRight: '5%',
+                    marginBottom: '5px',
+                    color: descricao.length >= subTaskLength ? 'red' : '#666',
+                }}
+            >
+                {descricao.length} / {subTaskLength}
+            </div>
             {mensagem ? <p className='alert alert-danger text-center msgErroSubTask' style={{ marginTop: '10px', padding:'10px', marginBottom:'0px' }}>{mensagem}</p> : ''}
             <button className='mx-auto btn btn-sm btn-success btnEntrar' type='submit'>Alterar</button>
         </form>
